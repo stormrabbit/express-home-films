d5db6c5e53831877d0e65ebec47a5dcae297dc8f
欧阳夏昱 <ouyangxiayu@m24.cn>
2017-12-02 21:54:41 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log('=============><==============')
+  console.log(req.query);
+  const {
+    id
+  } = req.query;
+
+  const temp = Object.keys(req.query).reduce( (pre, cur) => {
+      pre += `${cur} : ${req.query[cur]}  `
+      return pre;
+  }, ``)
+
+  res.send(`you are querying ${temp}`);
+});
+
+/* GET users listing. */
+router.get('/form', function(req, res, next) {
+  const temp = {
+    id: 'fuck',
+    childNode: 'input'
+  };
+  const tempStr = JSON.stringify(temp);
+  res.send(tempStr);
 });
 
 module.exports = router;
